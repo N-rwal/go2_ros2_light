de7c4fff0e42a5634fee5cee4c550fcecd67a437
N-rwal <liviuvalache@hotmail.com>
2026-01-20 17:12:36 +0100

    working barebones on pi 5

--- go2_robot_sdk/launch/2_mapping.launch.py
+++ go2_robot_sdk/launch/2_mapping.launch.py
@@ -8,6 +8,7 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
 
 
 def generate_launch_description():
